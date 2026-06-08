6182b1b32f07abc66224f8146cc276e6a87faff2
isabelladim <284634042+isabelladim@users.noreply.github.com>
2026-06-08 14:55:39 +1000

    API_connection update: 0 tip data retruned due to team_id parameter, update code

--- ingestion/API_connection.py
+++ ingestion/API_connection.py
@@ -1,14 +1,14 @@
 import requests
 import pandas as pd
 
-def get_api_info(url_extension, year, teams, user_agent):
-    response = requests.get(f"https://api.squiggle.com.au/?q={url_extension};year={year};team={teams}", headers={"User-Agent": user_agent})
+def get_team_games(year, teams, user_agent):
+    response = requests.get(f"https://api.squiggle.com.au/?q=games;year={year};team={teams}", headers={"User-Agent": user_agent})
+    data = response.json()
+    return pd.DataFrame(data["games"])
+
+def get_afl_info(url_extension, year, user_agent):
+    response = requests.get(f"https://api.squiggle.com.au/?q={url_extension};year={year}", headers={"User-Agent": user_agent})
     data = response.json()
     return pd.DataFrame(data[url_extension])
-
-def get_ladder_info(year, user_agent):
-    response = requests.get(f"https://api.squiggle.com.au/?q=standings;year={year}", headers={"User-Agent": user_agent})
-    data = response.json()
-    return pd.DataFrame(data["standings"])
 
 
