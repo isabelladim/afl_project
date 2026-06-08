63870f75c4b76b16d306fa425bb5161f8c35f7b5
isabelladim <284634042+isabelladim@users.noreply.github.com>
2026-06-08 17:55:32 +1000

    update upload_to_db to include current ladder

--- ingestion/upload_to_db.py
+++ ingestion/upload_to_db.py
@@ -43,6 +43,7 @@ def main():
     team_ID = 15 #St Kilda
     datasets = {
         "games": API_connection.get_team_games(year, team_ID, user_agent),
+        "standings": API_connection.get_afl_info("standings", year, user_agent),
         "ladder": API_connection.get_afl_info("ladder", year, user_agent),
         "teams": API_connection.get_afl_info("teams", year, user_agent),
         "tips": API_connection.get_afl_info("tips", year, user_agent)
