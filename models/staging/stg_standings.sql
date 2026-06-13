0cc69c503f6d776d311cd67045971f6f1feaa19b
isabelladim <284634042+isabelladim@users.noreply.github.com>
2026-06-13 16:30:09 +1000

    update quoted columns to capital in line with snowflake capitalisation

--- models/staging/stg_standings.sql
+++ models/staging/stg_standings.sql
@@ -1,10 +1,10 @@
 select 
-cast("name" as text) as team_name,
+cast("NAME" as text) as team_name,
 cast(rank as integer) as rank,
 cast(goals_for as integer) as goals_for,
 cast(draws as integer) as draws,
 cast(pts as integer) as points,
-cast("percentage" as float) as percentage,
+cast("PERCENTAGE" as float) as percentage,
 cast(id as text) as team_id,
 cast(behinds_for as integer) as behinds_for,
 cast(wins as integer) as wins,
@@ -13,6 +13,6 @@ cast(behinds_against as integer) as behinds_against,
 cast(against as integer) as against,
 cast(losses as integer) as losses,
 cast(played as integer) as played,
-cast("for" as integer) as for_points
+cast("FOR" as integer) as for_points
 
 from {{ source('raw', 'standings') }}
