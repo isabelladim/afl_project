9f3ddea8e333dbba08c1129d448c18e026f1eaae
isabelladim <284634042+isabelladim@users.noreply.github.com>
2026-06-13 16:24:21 +1000

    update main for snowflake ingestion

---
+++ ingestion/main.py
@@ -0,0 +1,37 @@
+import os 
+import pandas as pd
+import snowflake.connector.pandas_tools
+from dotenv import load_dotenv
+import upload_to_snowflake
+from upload_to_snowflake import get_snowflake_connection, upload_to_snowflake
+import logging
+import API_connection 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+#load env file
+load_dotenv()
+user_agent = os.environ["SQUIGGLE_USER_AGENT"]
+
+def main():
+    engion = get_snowflake_connection() #connect to snowflake
+    year = 2026
+    team_ID = 15 #St Kilda
+    datasets = {
+        "GAMES": API_connection.get_afl_info("games", year, user_agent),
+        "STANDINGS": API_connection.get_afl_info("standings", year, user_agent),
+        "LADDER": API_connection.get_afl_info("ladder", year, user_agent),
+        "TEAMS": API_connection.get_afl_info("teams", year, user_agent),
+        "TIPS": API_connection.get_afl_info("tips", year, user_agent)
+    }
+
+    for table_name, data in datasets.items():
+        upload_to_snowflake(data, table_name, engion)   #load data into snowflake
+        logger.info("Loaded %s rows into raw.%s", len(data), table_name)
+
+if __name__ == "__main__":
+    main()
