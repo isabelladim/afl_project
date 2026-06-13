cbefe7d892aad2588feffdd3e94722c75db87c92
isabelladim <284634042+isabelladim@users.noreply.github.com>
2026-06-13 15:40:49 +1000

    remove pandas from api_connection and keep only 1 get_info

--- ingestion/API_connection.py
+++ ingestion/API_connection.py
@@ -1,14 +1,8 @@
 import requests
-import pandas as pd
-
-def get_team_games(year, teams, user_agent):
-    response = requests.get(f"https://api.squiggle.com.au/?q=games;year={year};team={teams}", headers={"User-Agent": user_agent})
-    data = response.json()
-    return pd.DataFrame(data["games"])
 
 def get_afl_info(url_extension, year, user_agent):
-    response = requests.get(f"https://api.squiggle.com.au/?q={url_extension};year={year}", headers={"User-Agent": user_agent})
-    data = response.json()
-    return pd.DataFrame(data[url_extension])
-
+    url = f"https://api.squiggle.com.au/?q={url_extension};year={year}"
+    response = requests.get(url, headers={"User-Agent": user_agent}, timeout=30)
+    response.raise_for_status()
+    return response.json()[url_extension]
 
