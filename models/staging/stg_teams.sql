85c0b70e2c4e25fe464cb474b95ab9e9dd6ac1dc
isabelladim <284634042+isabelladim@users.noreply.github.com>
2026-06-14 14:59:47 +1000

    updating quoted column names to capital in line with snowflake column headings

--- models/staging/stg_teams.sql
+++ models/staging/stg_teams.sql
@@ -1,5 +1,5 @@
 select 
-cast("name" as text) as team_name,
+cast("NAME" as text) as team_name,
 cast(debut as bigint) as debut_year,
 cast(id as text) as team_id,
 cast(abbrev as text) as team_abbrev
